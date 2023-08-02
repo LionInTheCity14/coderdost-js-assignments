77d7094f61f5b7cf21f3dbf64e6a6dd2b5aed8af
Varun Pal Singh <singhvarun928@gmail.com>
2023-08-02 13:31:05 +0530

    completed chapter 5 HTML DOM

--- chapter5-dom/5.js
+++ chapter5-dom/5.js
@@ -1,5 +1,22 @@
+// **5.1:** Explain difference between **innerText** and **innerHTML** in the 
+// following example?
+
 let content = document.getElementById('content');
 
-console.log(content.innerHTML);
+console.log(content.innerHTML);     // will show all the HTML (elements also, if 
+// present) inside content element;     "        <h2>Hello Coders</h2>" , including spaces.
 
-console.log(content.innerText);
+console.log(content.innerText);     // will show all the text available inside 
+// content element;                     "Hello Coders"
+
+
+
+// just nonsense code lol.
+
+const var1 = document.createElement("p");
+var1.setAttribute("class", "new-class");
+console.log(var1.classList, var1.id);
+
+var1.classList.replace("new-class","new1-class");
+console.log(var1.classList, var1.id);
+
