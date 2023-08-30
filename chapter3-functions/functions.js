d76deb2482892d7ca2186a014d6267fe886435a6
varun <singhvarun928@gmail.com>
2023-08-30 08:54:29 +0530

    functions

---
+++ chapter3-functions/functions.js
@@ -0,0 +1,53 @@
+// function sayMyName(){
+//     console.log("V");
+//     console.log("A");
+//     console.log("R");
+//     console.log("U");
+//     console.log("N");
+// }
+
+// conosle.log(sayMyName);
+// sayMyName();
+
+function addTwoNum(num1, num2){
+    if(typeof num1 === "number" && typeof num2 === "number")
+        return num1 + num2;
+    else
+        return `enter correct numbers`;
+}
+
+// console.log(addTwoNum(2, 4));
+// console.log(typeof(5) === "number");
+
+function loginUserMessage(username){
+    // if(username === undefined)
+    if(!username){
+        console.log("Please enter a username");
+        return;
+    }
+    else
+        return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("varun"));
+console.log(loginUserMessage());
+
+// dont know how many params are given
+
+function calculateCartPrice(...nums){
+    return nums.reduce((acc, num) => num + acc, 0);
+}
+
+console.log(calculateCartPrice(200));
+console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "varun",
+    id: 1,
+}
+
+function handleObject(obj){
+    console.log(`username is ${obj.username} and id is ${obj.id}`);
+}
+
+handleObject(user);
