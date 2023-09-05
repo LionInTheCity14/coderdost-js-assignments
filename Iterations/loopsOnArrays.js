85f2da9cd35d12f6d221c2e5605cf479d359f54e
Varun Pal Singh <singhvarun928@gmail.com>
2023-09-05 14:30:28 +0530

    completed js-1 of hitesh sir

--- Iterations/loopsOnArrays.js
+++ Iterations/loopsOnArrays.js
@@ -38,12 +38,63 @@ const filtered = nums.filter((num) => {
     // console.log(num);
     return num > 4
 });
-console.log(filtered);
+// console.log(filtered);
 
 // map do some operation on each value of array and return it and add it to new array.
 const mapped = nums.map((elm) => {  
     return elm * 2;
 });
-console.log(mapped);
+// console.log(mapped);
 
-console.log(nums);  // original array is intect.
+// channing
+const mapped1 = nums
+                .map((elm) => elm * 3)
+                .filter((elm) => elm > 15);
+
+console.log(mapped1);
+
+console.log();
+
+// reduce does some operation on each element of array and return accumalated value
+
+const initialValue = 0;
+
+const reduced = nums.reduce((acc, currValue) => {
+    console.log(`acc : ${acc}, currValue : ${currValue}`);
+    return acc + currValue;
+}, initialValue);
+
+console.log(reduced);
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999,
+    },
+    {
+        itemName : "py course",
+        price : 999,
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999,
+    },
+    {
+        itemName : "data science course",
+        price : 12999,
+    },
+];
+
+const total = shoppingCart.reduce((acc, currItem) => acc + currItem.price, 0);
+
+console.log(total);
+// or 
+
+// const [itemName, price] = {itemName : "something", price : 999};
+// console.log(itemName, price);
+
+// const total1 = shoppingCart.reduce((acc, [name, price]) => acc + price, 0);
+
+// console.log(total1);
+
+console.log(nums);  // original array is not changed.
